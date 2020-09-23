8b2d98653a233efabbcde0531da18783e556faf1
rgj40q <45658038+rgj40q@users.noreply.github.com>
2020-09-23 16:01:55 +0700

    Add files via upload

--- util.js
+++ util.js
@@ -1,17 +1,27 @@
 function ast2html(ast) {
     var result = "";
-    if (ast.name) {
-        result += "<ul><li>" + ast.name + "</li>";
-    }
-    if (ast.children) {
-        for (const element of ast.children) {
-            if (element) {
-                result += ast2html(element);
+    if (Array.isArray(ast) {
+        if (ast) {
+            for (const element of ast) {
+                if (element) {
+                    result += ast2html(element);
+                }
             }
         }
-    }
-    if (ast.name) {
-        result += "</ul>";
+    } else {
+        if (ast.name) {
+            result += "<ul><li>" + ast.name + "</li>";
+        }
+        if (ast.children) {
+            for (const element of ast.children) {
+                if (element) {
+                    result += ast2html(element);
+                }
+            }
+        }
+        if (ast.name) {
+            result += "</ul>";
+        }
     }
     return result;
 }
