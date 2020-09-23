e48e3d21407633aa8d02727fbe38f1abe8bbafb1
rgj40q <45658038+rgj40q@users.noreply.github.com>
2020-09-23 15:11:40 +0700

    Add files via upload

--- util.js
+++ util.js
@@ -1,24 +1,27 @@
-fetch("grammar.pegjs").then(response => response.text()).then(grammar => {
-    let parser = peg.generate(grammar);
-    document.getElementById("button").onclick = error => {
-        try {
-            let ast = parser.parse(document.getElementById("text").value);
-            console.log(ast);
-            document.getElementById("result").innerHTML = "Parsed successfully: " + ast2html(ast);
-        } catch(error) {
-            document.getElementById("result").innerHTML = "Parsing error: " + error.message;
-        }
-    };
-});
-
 function ast2html(ast) {
     var result = "<ul>";
     result += "<li>" + ast.name + "</li>";
-    for (const element of ast.children) {
-        if (element) {
-            result += ast2html(element);
+    if (ast.children) {
+        for (const element of ast.children) {
+            if (element) {
+                result += ast2html(element);
+            }
         }
     }
     result += "</ul>";
     return result;
+}
+
+function button_onclick() {
+    var parser, grammar, ast, result, error, response;
+    response = fetch("grammar.pegjs");
+    grammar = response.text();
+    parser = peg.generate(grammar);
+    try {
+        ast = parser.parse(document.getElementById("text").value);
+        result = "Parsed successfully: " + ast2html(ast);
+    } catch(error) {
+        result = "Parsing error: " + error.message;
+    }
+    document.getElementById("result").innerHTML = result;
 }
