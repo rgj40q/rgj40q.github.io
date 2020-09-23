4e62e1943dce58d4348fe276b63ee5f05052ea4c
rgj40q <45658038+rgj40q@users.noreply.github.com>
2020-09-23 19:45:26 +0700

    Add files via upload

---
+++ dict.js
@@ -0,0 +1,9 @@
+// key: word stem, values: ULD#, english translation
+// ULD is http://www.frathwiki.com/Universal_Language_Dictionary
+
+const dict_array = [
+["", "", ""],
+["", "", ""]
+]
+
+const dict = new Map(dict_array)
--- util.js
+++ util.js
@@ -1,47 +1,45 @@
 function ast2html(ast) {
-    var result = "";
+    var result = ""
     if (Array.isArray(ast)) {
         if (ast) {
             for (const element of ast) {
                 if (element) {
-                    result += ast2html(element);
+                    result += ast2html(element)
                 }
             }
         }
     } else {
         if (ast.name) {
-            result += "<ul><li>" + ast.name + "</li>";
+            result += "<ul><li>" + ast.name + "</li>"
         }
         if (ast.children) {
             for (const element of ast.children) {
                 if (element) {
-                    result += ast2html(element);
+                    result += ast2html(element)
                 }
             }
         }
         if (ast.name) {
-            result += "</ul>";
+            result += "</ul>"
         }
     }
-    return result;
+    return result
 }
 
 function buildErrorMessage(e) {
     return e.location !== undefined
-      ? "Line " + e.location.start.line + ", column " + e.location.start.column + ": " + e.message
-      : e.message;
-  }
+        ? "line " + e.location.start.line + ", column " + e.location.start.column + ": " + e.message
+        : e.message
+}
 
 function button_onclick() {
-    var parser, ast, result, error;
-    fetch("grammar.pegjs").then(response => response.text()).then(grammar => {
-        parser = peg.generate(grammar);
-        try {
-            ast = parser.parse(document.getElementById("textarea").value);
-            result = "Parsed successfully: " + ast2html(ast);
-        } catch(error) {
-            result = "Parsing error: " + buildErrorMessage(error);
-        }
-        document.getElementById("result").innerHTML = result;
-    });
+    var parser, ast, result, error
+    parser = peg.generate(grammar)
+    try {
+        ast = parser.parse(document.getElementById("textarea").value)
+        result = "Parsed successfully: " + ast2html(ast)
+    } catch(error) {
+        result = "Parsing error: " + buildErrorMessage(error)
+    }
+    document.getElementById("result").innerHTML = result
 }
