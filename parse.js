b89e6366eb51ae423177cb36bd3f331b2cf564a6
rgj40q <45658038+rgj40q@users.noreply.github.com>
2020-09-23 09:36:14 +0700

    Add files via upload

--- parse.js
+++ parse.js
@@ -1,22 +1,14 @@
 function button_onclick() {
-    var response, grammar, text, result, parser, error, ast, ok;
+    var response, grammar, text, result, parser, error, ast;
     response = fetch("grammar.pegjs");
     grammar = response.text;
     text = document.getElementById("text").value;
-    ok = 1;
+    parser = peg.generate(grammar);
     try {
-        parser = peg.generate(grammar);
+        ast = parser.parse(text);
+        result = "Success! " + ast2html(ast);
     } catch(error) {
-        result = "Grammar error: " + error;
-        ok = 0;
-    }
-    if (ok) {
-        try {
-            ast = parser.parse(text);
-            result = "Success! " + ast2html(ast);
-        } catch(error) {
-            result = "Parsing error: " + error;
-        }
+        result = "Parsing error: " + error;
     }
     document.getElementById("result").innerHTML = result;
 }
