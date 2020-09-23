d15ad60f9a93e44985ac19f86055d9be40e97dee
rgj40q <45658038+rgj40q@users.noreply.github.com>
2020-09-23 15:22:44 +0700

    Add files via upload

--- util.js
+++ util.js
@@ -14,13 +14,14 @@ function ast2html(ast) {
 
 function button_onclick() {
     var parser, grammar, ast, result, error;
-    grammar = fetch("grammar.pegjs").text();
-    parser = peg.generate(grammar);
-    try {
-        ast = parser.parse(document.getElementById("text").value);
-        result = "Parsed successfully: " + ast2html(ast);
-    } catch(error) {
-        result = "Parsing error: " + error.message;
+    fetch("grammar.pegjs").then(response => response.text()).then(grammar => {
+        parser = peg.generate(grammar);
+        try {
+            ast = parser.parse(document.getElementById("text").value);
+            result = "Parsed successfully: " + ast2html(ast);
+        } catch(error) {
+            result = "Parsing error: " + error.message;
+        }
+        document.getElementById("result").innerHTML = result;
     }
-    document.getElementById("result").innerHTML = result;
 }
